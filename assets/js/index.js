a99f0c1615047adb66196b9599914e00f8359b29
Eric Wieser <wieser.eric@gmail.com>
2016-03-09 13:15:55 -0500

    Fix image resizing

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,7 +35,7 @@
     $document.ready(function () {
 
         // Fit all videos for post content
-        // var $postContent = $(".post-content");
+        var $postContent = $(".post-content");
         // $postContent.fitVids();
 
         // Fit all videos everywhere
